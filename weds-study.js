283a42863aa08b49d167945b636fab143916dac6
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-20 16:38:07 -0500

    going over objects and functions. data types.

---
+++ weds-study.js
@@ -0,0 +1,17 @@
+//
+//
+// You picked ...
+// How many camp sites are there?
+// What is a campsite?
+function campsiteCount() {
+
+}
+
+
+// Objects  --- > have methods and attributes/properties. Human. Shape.
+// Objects-- - > key value pairs
+// Attributes/Properties have value
+//
+{
+  hobbies: ["snowboaring", "hiking", "wakeboarding", "swimming"]
+}
--- working-with-soren-on-loops.js
+++ working-with-soren-on-loops.js
@@ -1,3 +1,57 @@
+// Functions
+
+// Data Types?
+// Strings  --- > "sequence of characters or numbers denoted by usally quotes"
+// Booleans --- > true or false values
+// Numbers  --- > 1,2,3
+
+// Arrays   --- > collections ["beef", "checking", 1, {}]
+// Objects  --- > have methods and attributes. Human. Shape.
+
+// casting data types
+
+// Function Definition
+// Function Declaration
+function doSomething() {
+  console.log("hello world");
+}
+
+// parameters or arguments
+// variables you can pass in to function
+function sorensFavoriteQuote(param1) {
+  console.log(param1);
+}
+
+// Functions can be called multiple times
+// This is called a FUNCTION CALL
+// doSomething();
+// doSomething();
+// doSomething();
+// doSomething();
+// doSomething();
+
+sorensFavoriteQuote("Just Keep Going!");
+
+function addTwoNums(param1, param2) {
+  console.log(param1 + param2);
+}
+
+addTwoNums("Dashawna:", " Master Full Stack Dev");
+// --------------------------------------------------
+
+// --------------------------------------------------
+//
+//
+
+function lowerCaseAndRemoveApostrophes(word) {
+  let wordLowerCased = word.toLowerCase();
+  wordLowerCased = wordLowerCased.replace(/'/, "s")
+  console.log(wordLowerCased);
+}
+
+lowerCaseAndRemoveApostrophes("SO'ME WO'RD NEE'DING LOWER''CASING");
+
+
 // console.log("Lets make 250,000,000");
 
 // for (let counter = 0; counter <= 20; counter += 2) {
