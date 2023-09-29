e523e227322b798addc50249c3f7ff64da617ae2
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-29 13:50:36 -0500

    adding fizzbuzz

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Beginning of Our Study
+
+## Current Goals
+- being able to work with objects arrays and
+really understanding different data types
+  - strings
+  - numbers
+  - boolean
+  - arrays
+  - object
+  - undefined
+  - null
+
+
---
+++ startOfOurStudyGuide/scope-and-logic/fizzbuzz.js
@@ -0,0 +1,25 @@
+// FizzBuzz Challenge in JavaScript
+
+// Write a JavaScript program that prints the numbers from 1 to 100. But for multiples of 3, print "Fizz" instead of the number, and for the multiples of 5, print "Buzz." For numbers which are multiples of both three and five, print "FizzBuzz."
+
+// Your task is to implement the FizzBuzz logic in JavaScript.Create a function fizzBuzz that takes no parameters and outputs the FizzBuzz sequence up to 100.
+
+// Here's a sample output for the first 15 numbers:
+
+// Copy code
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// Make sure to test your function with different inputs and ensure it produces the correct FizzBuzz sequence.
