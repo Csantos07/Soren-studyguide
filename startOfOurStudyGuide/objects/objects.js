552001d3456279dafe3210cba00990c8d1251a9c
Carlo Santos <rcarlosantos89@gmail.com>
2023-09-28 21:33:45 -0500

    challenges

---
+++ startOfOurStudyGuide/objects/objects.js
@@ -0,0 +1,59 @@
+// THIS IS AN OBJECT
+// OBJECTS ARE KEY VALUE PAIRS
+let property = {
+  size: "2 family home",
+  rooms: 6,
+  bathrooms: 2,
+  amenities: ["garage", "backyard", "lawn", "pool"],
+  area: {
+    schoolDistrict: "PS 118",
+    localGrocery: "6th Avenue",
+    library: "Library Blvd"
+  }
+}
+
+let arrayOfProperties = [
+  {
+    size: "2 family home",
+    rooms: 6,
+    bathrooms: 2,
+    amenities: ["garage", "backyard", "lawn", "pool"],
+    area: {
+      schoolDistrict: "PS 118",
+      localGrocery: "6th Avenue",
+      library: "Library Blvd"
+    }
+  },
+  {
+    size: "3 family home",
+    rooms: 7,
+    bathrooms: 3,
+    amenities: ["garage", "backyard", "lawn", "pool"],
+    area: {
+      schoolDistrict: "PS 118",
+      localGrocery: "6th Avenue",
+      library: "Library Blvd"
+    }
+  },
+  {
+    size: "studio home",
+    rooms: 1,
+    bathrooms: 1,
+    amenities: ["garage", "backyard", "lawn", "pool"],
+    area: {
+      schoolDistrict: "PS 118",
+      localGrocery: "6th Avenue",
+      library: "Library Blvd"
+    }
+  }
+
+]
+
+// Take note the difference between an object array
+// console.log(property.size);
+
+// Can you create a function that counts all the rooms available in
+// your properties
+function totalRoomsOnOurProperty(anArrayOfProperties) {
+  // this should return a integer value of all your rooms added together
+}
